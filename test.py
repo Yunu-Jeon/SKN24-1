68d84623b63795afcbbfb49052d84e8ab374da60
Yunu-Jeon <yunu2002@hufs.ac.kr>
2026-01-14 17:09:54 +0900

    재미있는 함수 만들기 ver1.0.0

--- test.py
+++ test.py
@@ -1,3 +1,10 @@
 import streamlit as st
 
 st.title('재미있는 함수 만들기')
+
+st.subtitle('재미있는 함수 만들기')
+
+st.subtitle('이제 수정사항을 반영하기 위해')
+st.text('내용을 추가해볼게요')
+
+print('물론 파일을 추가하시거나 삭제하시는 행위도 반영된답니다 :)')
